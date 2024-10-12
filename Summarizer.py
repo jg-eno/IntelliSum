a8cd30371528ab0200e3086dd34f749dabb50b29
jg-eno <glen23bcd61@iiitkottayam.ac.in>
2024-10-13 03:20:10 +0530

    Added Summarize.py. This contains the LLM for summarization. It gets the input as a JSON and outputs a JSON object as well

---
+++ Summarizer.py
@@ -0,0 +1,51 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel,Field
+from langchain_core.output_parsers import JsonOutputParser
+import json
+from flask import Flask, request
+
+class JsonCreater(BaseModel):
+    sender: str = Field(description="Sender of the mail")
+    content: str = Field(description="Content of the mail")
+
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro",google_api_key="")
+template = """ 
+You are a personal assisstant who summarizes the mail contents and provides a summary based on the given content.
+Respond with a JSON object using the following instructions : 
+{{
+    "sender":"<Sender of the email>"
+    "context":"<1 line describing the content of the email>"
+}}
+
+content : {content}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+parser = JsonOutputParser(pydantic_object=JsonCreater)
+
+chain = prompt | llm | parser
+
+app = Flask(__name__)
+@app.route("/summary",methods=['POST'])
+def summary():
+    content = request.json.get('content')
+    if request.method == 'POST':
+        try:
+            msg = chain.invoke({"content":content})
+
+            return(msg)
+
+        except json.JSONDecodeError as e:
+            return(f"Error parsing JSON : {e}")
+    
+    else:
+        return "Not in the specific request format"
+        
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
