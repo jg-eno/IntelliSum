69ccf52b52b16a53265465389c84ce6dcd4898b9
Arjun Manjunath <arjunmanjunath80@gmail.com>
2024-10-28 08:51:00 +0530

    Update Summarizer.py

--- Summarizer.py
+++ Summarizer.py
@@ -1,19 +1,20 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_community.document_loaders.parsers.pdf import PDFMinerParser
-from langchain_community.document_loaders.parsers.generic import MimeTypeBasedParser
 from langchain_community.document_loaders.parsers.txt import TextParser
 from langchain_community.document_loaders import Blob
-import magic
 from langchain_core.messages import HumanMessage
 from pymongo import MongoClient
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
+import base64
+
+
 
 class JsonCreater(BaseModel):
-    sender: str = Field(description="Sender of the email")
+    title: str = Field(description="Sender of the email")
     context: str = Field(description="Context of the email content")
 
 # MongoDB setup
@@ -30,10 +31,13 @@ HANDLERS = {
 
 # Language model setup
 print("Setting up the language model...")
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key="")
-template = """ 
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key="")
+template = """
 You are a personal assistant who summarizes the mail contents and provides a summary based on the given content.
-Respond with a JSON object using the following instructions: 
+The content might includes text extracted from pdf and plain text files, that follows the format Attachments: <filename>, content: <extracted text>
+and also images might be uploaded
+
+Respond with a JSON object using the following instructions:
 {{
     "sender":"<Sender of the email>",
     "context":"<1 line describing the content of the email>"
@@ -41,7 +45,13 @@ Respond with a JSON object using the following instructions:
 
 content: {content}
 """
-prompt = ChatPromptTemplate.from_template(template)
+
+
+
+
+
+message = HumanMessagePromptTemplate.from_template(template)
+prompt = ChatPromptTemplate.from_messages([template])
 parser = JsonOutputParser(pydantic_object=JsonCreater)
 chain = prompt | llm | parser
 print("Language model setup complete.")
@@ -54,12 +64,11 @@ app.config['UPLOAD_EXTENSIONS'] = []
 @app.route("/summary", methods=['POST'])
 def summary():
     print("Received request at /summary endpoint.")
+    request_data = json.loads(request.form.get('json', ''))
+    print(type(request_data))
+    emails = request_data['emails']
+    user = request_data['user']
 
-    user = request.json.get('user')
-    emails = request.json.get('emails')  # Expecting a list of dictionaries with sender and content keys
-    json = json.loads(request.form.get('json'))
-
-    
     if not isinstance(emails, list):
         print("Invalid input: 'emails' should be a list.")
         return jsonify({"message": "Expected list of emails"}), 400
@@ -76,31 +85,31 @@ def summary():
             images = []
             for filename in files:
                 file = request.files.get(filename, 0)
-                if file == 0: 
+                mime_type = file.content_type
+                if file == 0:
                     return jsonify({'message': 'Filename mismatch in json and form'}), 400
                 data = file.read()
-                mime = magic.Magic(mime=True)
-                mime_type = mime.from_buffer(data)
                 blob = Blob.from_data(data=data, mime_type=mime_type)
                 if mime_type in HANDLERS:
                     parser = HANDLERS[mime_type]
                     attachment_content = parser.parse(blob=blob)[0]
                     content += f"Attachment: {file.filename}, content: {attachment_content.page_content}"
                 elif 'image' in mime_type:
-                    images.append(base64.b64encode(file.read()).decode("utf-8"))
+                    images.append({'mime_type': mime_type, 'data': base64.b64encode(data).decode("utf-8")})
 
             message = HumanMessage(
                 content=[
                     {"type": "text", "text": content},
                     *[{
                         "type": "image_url",
-                        "image_url": {"url": f"data:image/jpeg;base64,{image_data}"},
-                    } for image_data in images
+                        "image_url": {"url": f"data:{image['mime_type']};base64,{image['data']}"},
+                    } for image in images
                     ]
                 ]
             )
+            print(message.content)
             print(f"Summarizing email {index + 1} from {sender}...")
-            msg = chain.invoke(message, timeout=5)
+            msg = chain.invoke({'content': message.content}, timeout=5)
             summaries.append({"user": user, "sender": sender, "context": msg["context"]})
             print(f"Summary for email {index + 1}: {msg['context']}")
 
@@ -124,4 +133,4 @@ def summary():
 
 if __name__ == "__main__":
     print("Starting Flask server...")
-    app.run()
+    app.run(debug=True)
